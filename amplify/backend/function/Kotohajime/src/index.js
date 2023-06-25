0311962a282dcded24e153ea43a255069c7a05e9
m19e <nexus.me7290@gmail.com>
2023-06-25 15:21:10 +0900

    push function to lambda

--- amplify/backend/function/Kotohajime/src/index.js
+++ amplify/backend/function/Kotohajime/src/index.js
@@ -11,18 +11,25 @@
 	STORAGE_S3KOTOHAJIMESTORAGEFA52AE71_BUCKETNAME
 Amplify Params - DO NOT EDIT */
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const AWS = require("aws-sdk")
+const table = "Board-ypmayfwctbbdzh3k2prsrzw3pe-staging"
+
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async event => {
+  const db = new AWS.DynamoDB.DocumentClient()
+  const data = await db.scan({ TableName: table }).promise()
+
   console.log(`EVENT: ${JSON.stringify(event)}`)
   return {
     statusCode: 200,
     //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
-    body: JSON.stringify("Hello from Lambda!"),
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "*",
+    },
+    body: JSON.stringify(data),
   }
 }
