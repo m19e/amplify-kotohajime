3c966869197f18165c47d960ea41e8c973232cb8
m19e <nexus.me7290@gmail.com>
2023-06-25 16:48:19 +0900

    feat: get board datas from lambda => dynamodb

--- src/pages/lambda.tsx
+++ src/pages/lambda.tsx
@@ -1,5 +1,7 @@
 import useSWR from "swr"
 
+import type { LazyBoard } from "src/models"
+
 const ENDPOINT =
   "https://kxnshqwisi.execute-api.ap-northeast-1.amazonaws.com/default/Kotohajime-staging"
 
@@ -9,7 +11,17 @@ const Lambda = () => {
 
 const fetcher = async () => {
   const res = await fetch(ENDPOINT)
-  return (await res.json()) as string
+  const { Items } = (await res.json()) as { Items: LazyBoard[] }
+
+  return (
+    <ul className="join-vertical join w-full rounded bg-white shadow">
+      {Items.map(b => (
+        <li key={b.id} className="join-item border px-4 py-2">
+          {b.message} ({b.name})
+        </li>
+      ))}
+    </ul>
+  )
 }
 
 const Content = () => {
@@ -20,7 +32,7 @@ const Content = () => {
   }
 
   return (
-    <div className="flex flex-col items-center border-2">
+    <div className="flex flex-col items-center gap-2 border-2 p-4">
       <h5 className="text-lg font-semibold">[Lambda result]</h5>
       <div>{data}</div>
     </div>
